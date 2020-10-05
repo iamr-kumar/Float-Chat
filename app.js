cbfca6cf019370b1422fe30299698dbd016af593
Ritik Kumar <ritik.kumar006@gmail.com>
2020-10-05 11:29:00 +0530

    Added comments

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const connectDb = require("./config/db");
 const socket = require("socket.io");
 const session = require("express-session");
 
+// Connect to db
 connectDb();
 
+// Bring in routes
 const userRoutes = require("./routes/users");
 const indexRoutes = require("./routes/index");
 
+// Middleware setup
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
@@ -22,8 +25,10 @@ app.use(
 
 app.set("view engine", "ejs");
 
+// Global variable to store active users
 global.users = [];
 
+// Use express router
 app.use(userRoutes);
 app.use(indexRoutes);
 
@@ -33,9 +38,10 @@ const server = app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
 
-
+// Initialize socket
 const io = socket(server);
 
+// Make sure that a user exists before making a connection
 io.use((socket, next) => {
   if(socket.handshake.query.username) {
     return next();
@@ -43,26 +49,32 @@ io.use((socket, next) => {
   return next(new Error('Authentication error!'));
 });
 
+
+// Establish connection
 io.on("connection", (socket) => {
   // console.log("Made new connection");
   const token = socket.handshake.query.username;
+  // On disconnect, remove this user from active user's list
   socket.on('disconnect', () => {
     const clientId = socket.id;
     users = users.filter(user => user.id !== clientId);
   });
 
+  // Add the user to active user's list
   users.push({
     id: socket.id,
     name: token
   });
 
+  // Get message data from client, find the reciever's socket id, and emit to that specific id
   socket.on('message', (data) => {
     const reciever = data.to;
     const recieverId = users.find(user => user.name === reciever);
-    console.log(recieverId);
+    // console.log(recieverId);
     io.to(recieverId.id).emit('message', data);
   });
 
+  // Broadcast new user to all users except the current user
   socket.broadcast.emit('newUser', {
     id: socket.id,
     name: token
